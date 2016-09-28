fdad690272ad5fde38ba84c7df4fd74a66c7360a
wuppious <nico.casper@hotmail.com>
2016-09-29 02:02:40 +0300

    Tabs to spaces and efficiency

--- wavzpy.py
+++ wavzpy.py
@@ -7,7 +7,7 @@ SCALE = HEIGHT / 2.5 / 32768
 
 DATA_SIZE = 2048
 
-filename = 'dustsucker.wav'
+filename = 'plus1.wav'
 
 goneTime = 0
 startTime = 0
@@ -16,80 +16,82 @@ offset = 0
 pygame.init()
 pygame.display.set_caption('WaVz - WAVE format audio visualizer')
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-
+clock = pygame.time.Clock()
 running = True
 
 
 with open(filename, 'rb') as f:
 
-	RIFF_chunkID 		= int.from_bytes(f.read(4), byteorder='big')
-	RIFF_chunkSize 		= int.from_bytes(f.read(4), byteorder='little')
-	RIFF_format 		= int.from_bytes(f.read(4), byteorder='big')
+    RIFF_chunkID        = int.from_bytes(f.read(4), byteorder='big')
+    RIFF_chunkSize      = int.from_bytes(f.read(4), byteorder='little')
+    RIFF_format         = int.from_bytes(f.read(4), byteorder='big')
 
-	fmt_subchunk1ID 	= int.from_bytes(f.read(4), byteorder='big')
-	fmt_subchunk1Size 	= int.from_bytes(f.read(4), byteorder='little')
-	fmt_audioFormat 	= int.from_bytes(f.read(2), byteorder='little')
-	fmt_numChannels 	= int.from_bytes(f.read(2), byteorder='little')
-	fmt_sampleRate 		= int.from_bytes(f.read(4), byteorder='little')
-	fmt_byteRate 		= int.from_bytes(f.read(4), byteorder='little')
-	fmt_blockAlign 		= int.from_bytes(f.read(2), byteorder='little')
-	fmt_bitsPerSample 	= int.from_bytes(f.read(2), byteorder='little')
+    fmt_subchunk1ID     = int.from_bytes(f.read(4), byteorder='big')
+    fmt_subchunk1Size   = int.from_bytes(f.read(4), byteorder='little')
+    fmt_audioFormat     = int.from_bytes(f.read(2), byteorder='little')
+    fmt_numChannels     = int.from_bytes(f.read(2), byteorder='little')
+    fmt_sampleRate      = int.from_bytes(f.read(4), byteorder='little')
+    fmt_byteRate        = int.from_bytes(f.read(4), byteorder='little')
+    fmt_blockAlign      = int.from_bytes(f.read(2), byteorder='little')
+    fmt_bitsPerSample   = int.from_bytes(f.read(2), byteorder='little')
 
-	data_subchunk2ID 	= int.from_bytes(f.read(4), byteorder='big')
-	data_subchunk2Size 	= int.from_bytes(f.read(4), byteorder='little')
+    data_subchunk2ID    = int.from_bytes(f.read(4), byteorder='big')
+    data_subchunk2Size  = int.from_bytes(f.read(4), byteorder='little')
 
 
 
-	if not fmt_audioFormat:
-		print('Compression format not supported')
-		f.close()
-		running = False
+    if not fmt_audioFormat:
+    	print('Compression format not supported')
+    	f.close()
+    	running = False
 
-	pygame.mixer.music.load(filename)
-	pygame.mixer.music.play()
+    pygame.mixer.music.load(filename)
+    pygame.mixer.music.play()
 
-	startTime = time.clock()
+    startTime = time.clock()
 
-	while running:
+    while running:
 
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				running = False
-			if event.type == pygame.KEYUP:
-				if event.key == pygame.K_UP:
-					DATA_SIZE = min(16384, DATA_SIZE * 2)
-				elif event.key == pygame.K_DOWN:
-					DATA_SIZE = max(128, DATA_SIZE // 2)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_UP:
+                DATA_SIZE = min(16384, DATA_SIZE * 2)
+                elif event.key == pygame.K_DOWN:
+                DATA_SIZE = max(128, DATA_SIZE // 2)
+                elif event.key == pygame.K_SPACE:
+                pygame.mixer.music.stop()
+                pygame.mixer.music.play()
+                startTime = time.clock()
 
-		screen.fill((0,0,0))
-		goneTime = time.clock() - startTime
-		offset = 44 + int(fmt_byteRate * goneTime)
-		if offset & 1:
-			offset += 1
+    	screen.fill((0,0,0))
+        goneTime = time.clock() - startTime
+        offset = 44 + int(fmt_byteRate * goneTime)
 
-		f.seek(offset)
+    	f.seek(offset + (offset & 1))
 
-		points = []
-		for i in range(DATA_SIZE):
+    	points = []
+        for i in range(DATA_SIZE):
 
-			data = int.from_bytes(f.read(fmt_bitsPerSample // 8), byteorder='little')
+            data = int.from_bytes(f.read(fmt_bitsPerSample // 8), byteorder='little')
 
-			if data < 32768:
-				data += 32768
-			elif data > 32768:
-				data -= 32768
+            if data < 32768:
+                    data += 32768
+                    elif data > 32768:
+                    data -= 32768
 
-			x = (i / DATA_SIZE) * WIDTH
-			y = (data + 32768/2) * SCALE  
-			points.append((int(x),int(y)))
+            x = (i / DATA_SIZE) * WIDTH
+                    y = (data + 32768/2) * SCALE  
+                    points.append((int(x),int(y)))
 
-		lines = []
-		for x in range(WIDTH):
-			lines.append(points[DATA_SIZE * x // WIDTH])
+    	lines = []
+            for x in range(WIDTH):
+            lines.append(points[DATA_SIZE * x // WIDTH])
 
-		pygame.draw.lines(screen, (255,255,255), False, lines)
-		
+    	pygame.draw.lines(screen, (255,255,255), False, lines)
 
-		pygame.display.flip()
-		clock.tick(60)
-	f.close()
+
+    	pygame.display.flip()
+            clock.tick(60)
+            f.close()
